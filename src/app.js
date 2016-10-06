cfc7722b027edeacc30231a64b95af964b5e2da2
Femi Oladeji <femidotexe@gmail.com>
2016-10-06 23:16:42 +0100

    set up the interface

---
+++ src/app.js
@@ -0,0 +1,11 @@
+/*
+The file is concerned about maintaining the angular
+part of the app
+*/
+var invApp = angular.module('invertedIndex', []);
+
+invApp.directive('fileModel')
+
+invApp.controller('invertedController', function($scope){
+        
+})
