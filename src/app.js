0c15259bbbcc78f151fb08e35c25175f7d68cf35
Femi Oladeji <femidotexe@gmail.com>
2016-10-07 02:16:10 +0100

    started implementing the index class

--- src/app.js
+++ src/app.js
@@ -1,11 +1,58 @@
+"use strict";
 /*
 The file is concerned about maintaining the angular
 part of the app
 */
 var invApp = angular.module('invertedIndex', []);
 
-invApp.directive('fileModel')
+invApp.directive('fileModel', ['$parse', function($parse) {
+    var fileModelObject = {
+        restrict: 'A',
+        /**
+        link function is responsible for monitoring changes in the file field
+        @param {object} scope
+        @param {array} element - array of objects of all the input fields
+        with file-model attribute
+        @param {array} attrs - array of objects containing the attributes
+        of the input fields that has the attribute file-model
+        */
+        link: function(scope, element, attrs) {
+            // console.log(attrs);
+            var theFile = $parse(attrs.fileModel);
+            var modelSetter = theFile.assign;
+
+            element.bind('change', function() {
+                scope.$apply(function() {
+                    modelSetter(scope, element[0].files[0]);
+                });
+            });
+        }
+    };
+
+    return fileModelObject;
+}]);
+
+invApp.service('uploadfile', ['$http', function($http) {
+    this.uploadFileUrl = function(file, uploadUrl) {
+        var myFormData = new FormData;
+        myFormData.append('uploadedjsonfile', file);
+        $http.post(uploadFileUrl, myFormData, {
+            transformRequest: angular.identity,
+            headers: {'Content-Type': undefined}
+        })
+        .success(function() {
+
+        })
+        .error(function() {
+
+        })
+    };
+}]);
 
 invApp.controller('invertedController', function($scope){
-        
-})
+    $scope.getIndex = function() {
+        console.log('button clicked');
+        var invIndex = new Index();
+        invIndex.createIndex('dslkf');
+    }
+});
--- src/inverted-index.js
+++ src/inverted-index.js
@@ -1,1 +1,50 @@
 "use strict";
+var jsonData = [
+      {
+        "title": "Alice in Wonderland",
+        "text": "Alice falls into a rabbit hole and enters a world full of imagination."
+      },
+
+      {
+        "title": "The Lord of the Rings: The Fellowship of the Ring.",
+        "text": "An unusual alliance of man, elf, dwarf, wizard and hobbit seek to destroy a powerful ring."
+      }
+    ];
+
+
+var Index = function() {
+
+  /**
+    createIndex function is used to get all the index
+    @param {string} filepath- the path to the json file
+  */
+  this.createIndex = function(filepath) {
+    if(jsonData === null || jsonData.length === 0) {
+      return false;
+    }
+
+    var textArray = [], invalidDocs = [], documentNum = 1;
+
+    for(var eachDoc in jsonData) {
+      if(jsonData[eachDoc].hasOwnProperty('text') && 
+        jsonData[eachDoc].hasOwnProperty('title')) {
+          var textTokens = this.tokenize(jsonData[eachDoc].text);
+          textArray.push({documentNum, textTokens});
+      } else {
+        invalidDocs.push(documentNum);
+      }
+      documentNum++;
+    }
+
+    var wordIndex = {}
+  }
+
+  /**
+    tokenize: method converts the text to lowercase and then returns the
+    array of words
+    @param {string} text- the text to be tokenized
+  */
+  this.tokenize = function(text) {
+    return text.toLowerCase().split(" ");
+  }
+};
