5abc38e9d789312ca5798492606efcb69cd65c4b
Femi Oladeji <femidotexe@gmail.com>
2016-10-11 00:15:42 +0100

    add select box to track uploads

--- src/app.js
+++ src/app.js
@@ -5,63 +5,40 @@ part of the app
 */
 var invApp = angular.module('invertedIndex', []);
 
-invApp.directive('fileModel', ['$parse', function($parse) {
-    var fileModelObject = {
-        restrict: 'A',
-        /**
-        link function is responsible for monitoring changes in the file field
-        @param {object} scope
-        @param {array} element - array of objects of all the input fields
-        with file-model attribute
-        @param {array} attrs - array of objects containing the attributes
-        of the input fields that has the attribute file-model
-        */
-        link: function(scope, element, attrs) {
-            // console.log(attrs);
-            var theFile = $parse(attrs.fileModel);
-            var modelSetter = theFile.assign;
 
-            element.bind('change', function() {
-                scope.$apply(function() {
-                    modelSetter(scope, element[0].files[0]);
-                });
-            });
-        }
-    };
+invApp.controller('invertedController', ['$scope', function($scope){
+  $scope.uploadedFiles = {};
+  $scope.getIndex = function() {
+    var invIndex = new Index();
+    invIndex.createIndex(uploadedFiles);
+    // invIndex.createIndex('books.json', function() {
+    //   $scope.indexes = invIndex.getIndex();
+    //   $scope.documents = invIndex.getDocuments();
+    // });
+  }
 
-    return fileModelObject;
-}]);
+  document.getElementById('uploadfile').addEventListener('change', readJson);
 
-invApp.service('uploadfile', ['$http', function($http) {
-    this.uploadFileToUrl = function(file, uploadUrl) {
-        var myFormData = new FormData;
-        myFormData.append('uploadedjsonfile', file);
-        $http.post(uploadUrl, myFormData, {
-            transformRequest: angular.identity,
-            headers: {'Content-Type': 'multipart/form-data'}
-        })
-        .success(function() {
-          console.log('success');
-        })
-        .error(function() {
-          console.log('error');
-        })
-    };
-}]);
-
-invApp.controller('invertedController', ['$scope', 'uploadfile',
-  function($scope, uploadfile){
-    $scope.getIndex = function() {
-        var invIndex = new Index();
-        invIndex.createIndex('books.json', function() {
-          $scope.indexes = invIndex.getIndex();
-          $scope.documents = invIndex.getDocuments();
-        });
+  /**
+  readJson function is used to read the content of a file
+  @param {object} dom is an object representing the dom element the change event
+  was added to
+  */
+  function readJson(dom) {
+    var fileDetails = dom.target.files[0];
+    //check if filename ends in json
+    if(!fileDetails.name.match(/\.json$/)) {
+      alert('Invalid file. You can only upload JSON');
+    } else {
+      var readFile = new FileReader();
+      readFile.readAsText(fileDetails);
+      readFile.onload = function(file) {
+        var content = file.target.result;
+        $scope.uploadedFiles[fileDetails.name] = JSON.parse(content);
+        //to make angular update the view
+        $scope.$apply();
+      }
     }
-
-    $scope.performupload = function() {
-      var file = $scope.jsonfile;
-      console.log(file);
-      uploadfile.uploadFileToUrl(file, '/files');
-    }
+    console.log($scope.uploadedFiles);
+  }
 }]);
