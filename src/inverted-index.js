db578895187dadc02a175353b0768b28e4e4b653
Femi Oladeji <femidotexe@gmail.com>
2016-10-12 21:41:38 +0100

    proper documentation via comments

--- src/inverted-index.js
+++ src/inverted-index.js
@@ -5,6 +5,7 @@ var Index = function() {
   /**
     createIndex function is used to get all the index
     @param {object} jsonData- the json data to index
+    @return {boolean} true or false if the createIndex was successful
   */
   this.createIndex = function(jsonData) {
     //checks if the jsondata is accurate and not empty
@@ -41,7 +42,10 @@ var Index = function() {
       }
       documentNum++;
     }
+    //saves the number of valid documents
     this.numOfDocs = documentNum;
+    //adds the attribute wordIndex to the class instance if the constructIndex
+    //was successful
     this.wordIndex = this.constructIndex(textArray);
   }
 
@@ -49,6 +53,7 @@ var Index = function() {
     tokenize: method converts the text to lowercase and then returns the
     array of words
     @param {string} text- the text to be tokenized
+    @return {array} array of words in the documents
   */
   this.tokenize = function(text) {
     text = text.replace(/[.,\/#!$%\^&\*;:'{}=\-_`~()]/g, "");
@@ -60,28 +65,27 @@ var Index = function() {
     and identifies the words in each
     @param {array} documents - array of objects with each obect representing
     a document
-
-
+    @return {object} objects of tokens. Each token is a key in the object and
+    contains an array of documents in which it was found
   */
   this.constructIndex = function(documents) {
     var indexDict = {};
+    //loop through the documents
     for(var each in documents) {
       var tokenArray = documents[each].textTokens;
       for(var i = 0; i < tokenArray.length; i++){
         var token = tokenArray[i];
-        //check if the word has already been indexed
+        //check if the word has not been indexed and used as key in the object
         if(!indexDict.hasOwnProperty(token)) {
-          //the token is used as a key and initialized to an array
-          //the first element of the array is the index of the document
-          indexDict[token] = [documents[each].documentNum];
-        } else {
-          //if the word has already been indexed
-          //a check is run to confirm if the document has been indexed
-          //with the word
-          if(indexDict[token].indexOf(documents[each].documentNum) === -1) {
-            indexDict[token].push(documents[each].documentNum);
-          }
+          //the token is used as a key and initialized to an empty array
+          indexDict[token] = [];
         }
+        //a check is run to confirm if the document has been indexed
+        //with the word
+        if(indexDict[token].indexOf(documents[each].documentNum) === -1) {
+          indexDict[token].push(documents[each].documentNum);
+        }
+        
       }
     }
 
