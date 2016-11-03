278782bc91fcccffbf2d21bf879c69db14e18eea
Femi Oladeji <femidotexe@gmail.com>
2016-11-03 12:38:44 +0100

    Add latest node version to travis

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,13 @@ const gulp = require('gulp'),
   source = require('vinyl-source-stream'),
   connect = require("gulp-connect");
 
-gulp.task('watch', function () {
+gulp.task('watch', () => {
   gulp.watch('src/*.js', ['reload']);
   gulp.watch('css/*.css', ['reload']);
   gulp.watch('*.html', ['reload']);
 });
 
-gulp.task('test-watch', function () {
+gulp.task('test-watch', () => {
   gulp.watch('jasmine/spec/*.js', ['test-reload']);
 });
 
@@ -20,7 +20,7 @@ gulp.task('test-reload', function () {
   gulp.src(['jasmine/spec/*.js']).pipe(connect.reload());
 });
 
-gulp.task('connect', function () {
+gulp.task('connect', () => {
   connect.server({
     root: '.',
     livereload: true,
@@ -28,18 +28,18 @@ gulp.task('connect', function () {
   });
 });
 
-gulp.task('browserify', function() {
+gulp.task('browserify', () => {
   return browserify('./spec/inverted-index-test-spec.js')
     .bundle()
     .pipe(source('test-spec.js'))
     .pipe(gulp.dest('./spec'));
 });
 
-gulp.task('test', ['browserify'], function () {
+gulp.task('test', ['browserify'], () => {
   run('node_modules/karma/bin/karma start karma.conf.js --single-run').exec();
 });
 
-gulp.task('reload', function () {
+gulp.task('reload', () => {
   gulp.src(['*.html', 'src/*.js', 'css/*.css']).pipe(connect.reload());
 });
 
